c92980cb96ae40abffa5e3419fb836f5b20e4910
Arnav Raj <arnavrajcodes@gmail.com>
2025-07-09 14:02:42 +0530

    user controller updated

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,42 @@
+import User from "../models/users.schema.js";
+import uploadToCloudinary from "../utils/cloudinary.js";
+
+const postUser = async (req, res) => {
+    try {
+        const input = req.body
+        const inputFile = req.files?.image;
+        let imgUrl = "";
+        if (inputFile) {
+            const filePath = file.tempFilePath;
+            imgUrl = uploadToCloudinary(filePath,'profile');
+        }
+        const userName = input.username
+        const user = await User.findOne({username:userName})
+        if(user){
+            res.status(400).json("username is taken. Please try again with another username")
+            return;
+        }
+        const newUser = new User({
+            username: userName,
+            name:input.name,
+            profilePic:imgUrl
+
+        })
+        await newUser.save()
+        res.status(200).json(newUser);
+    } catch (error) {
+        console.log("Error in posting user:\n", error);
+    }
+};    
+
+
+const getUser = async(req,res)=>{
+    const id = req.params.id
+    try {
+        res.status(200).json(`${id}`)
+    } catch (error) {
+        console.log("Error in getting user:\n", error);
+    }
+}
+
+export {postUser,getUser}
